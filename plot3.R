46afeb59942fdfaaf1eb334e0e8549dda36f4d34
Hadi Panahi <panahi@gmail.com>
2019-11-05 15:37:06 -0600

    change to plot 3

--- plot3.R
+++ plot3.R
@@ -11,8 +11,8 @@ data$Date <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 png("plot3.png", width=480, height=480)
 
 with(data, plot(Date, Sub_metering_1, type = "l", xlab = "", ylab =  "Energy sub metering"))
-with(data, lines(Date, Sub_metering_2, col = "blue"))
-with(data, lines(Date, Sub_metering_3,  col = "red"))
+with(data, lines(Date, Sub_metering_2, col = "red"))
+with(data, lines(Date, Sub_metering_3,  col = "blue"))
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 1, col = c("black", "red", "blue"))
 
 dev.off()
